2dc8487be37425ef953d58cea18ece7460b7ac56
Pablo Benedix <pabling.benedix@gmail.com>
2021-10-11 12:22:51 -0300

    Modelo de Base de Datos del usuarios agregado

--- servidor/pagina/models.py
+++ servidor/pagina/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class usuarios(models.Model):
+    id_usuario=models.AutoField(primary_key=True)
+    usuario=models.CharField(max_length=10)
+    clave=models.CharField(max_length=10)
+    nombre_completo=models.CharField(max_length=200)
