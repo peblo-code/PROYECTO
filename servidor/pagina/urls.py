4c717afaa0f27ae1e25cc908d1e3af9d5d21c56b
Pablo Benedix <pabling.benedix@gmail.com>
2021-12-10 10:07:22 -0300

    modalInfo

--- servidor/pagina/urls.py
+++ servidor/pagina/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('salir', views.salir, name='salir'),
     path('productos', views.productos, name='productos'),
     path('edit_product/<int:product_actual>', views.edit_product, name="edit_product"),
+    path('modal_info/<int:product_actual>', views.modal_info, name='modal_info'),
     path('delete_product/<int:product_actual>', views.delete_product, name='delete_product'),
     path('mark_and_model/<int:marcaModelo_actual>/<int:tipo_carga>', views.mark_and_model, name='mark_and_model'),
     path('informes', views.informes, name='informes'),
