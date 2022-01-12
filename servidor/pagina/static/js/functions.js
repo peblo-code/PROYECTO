4c881937c015efad7904724b0aaa88bd658bc277
Pablo Benedix <pabling.benedix@gmail.com>
2022-01-12 07:39:15 -0300

    Validar campos en modal editar

--- servidor/pagina/static/js/functions.js
+++ servidor/pagina/static/js/functions.js
@@ -9,3 +9,28 @@ window.onload = function(){
       mes='0'+mes //agrega cero si el menor de 10
     document.getElementById('fechaActual').value=ano+"-"+mes+"-"+dia;
 }
+
+function validarFormularioEdicion() {
+  let inputs = document.getElementsByClassName("form-control")
+
+  document.getElementById("btnCarga")
+  .addEventListener("click", (evt) => {
+      let flag = true
+      for(let i=0; i < inputs.length; i++) {
+          if(inputs[i].value === '') {
+              alerta(argsValidacion)
+              flag = false
+              i = inputs.length
+          }
+      } if(flag) {
+          return alertaConfirmar('',argsModal)
+      }
+
+  })
+
+  var argsValidacion = {
+    TITLE: 'Ups!', 
+    TEXT: 'Por favor, complete todos los campos',
+    ICON: 'error', 
+  }
+}
